2001ec0782205eda9505dfab8673c54e8e93efb3
Yılmaz Ev <yilmaz.ev@outlook.com>
2025-03-25 23:06:58 +0300

    bug-fix: enable unoptimized images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     domains: ["ohbleegp4mjqz9wj.public.blob.vercel-storage.com"],
   },
   webpack(config) {
